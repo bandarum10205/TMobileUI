8781e9de4718ea57fbc511d72e9d89296599e8ae
ANU <gkotte@tenna.com>
2020-07-12 17:38:07 +0530

    added e2e testing

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList, updateReadingList, getAllBooks } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, updateReadingList } from '@tmo/books/data-access';
 import { StorageService } from '@tmo/shared/storage';
 import { Book, ReadingListItem } from '@tmo/shared/models';
 // const KEY = '[okreads API] Reading List';
@@ -9,17 +9,10 @@ import { Book, ReadingListItem } from '@tmo/shared/models';
   templateUrl: './reading-list.component.html',
   styleUrls: ['./reading-list.component.scss']
 })
-export class ReadingListComponent implements OnInit {
+export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
   date = new Date().toISOString();
   constructor(private store: Store) {}
-
-  ngOnInit(): void {
-    // this.store.select(getReadingList).subscribe(books => {
-      //this.readingList$ = books;
-    //});
-  }
-
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
